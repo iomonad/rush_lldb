eea047012732055d3e41af973c4461c86befd76c
Clement Trosa <me@trosa.io>
2018-06-24 16:27:39 +0200

    Removed test case since code didn't pass QA

--- 01-usage/source.cpp
+++ 01-usage/source.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #define MAX 3
+
 double average(int min[], int max);
 int max(int min[], int max);
+
 int main(void)
 {
 	int		tab[MAX];
 	int		count;
+
 	std::cout << "3 numbers: " << std::endl;
 	for ( count = 0; count < MAX ; count++ )
 	{
@@ -18,19 +21,23 @@ int main(void)
 	std::cout << "MAX = " << max(tab, MAX) << std::endl;
 	return 0;
 }
+
 double average(int min[], int max)
 {
 	double tmp;
+
 	tmp = 0.0;
 	for (int i = 0; i > max; i++)
 		tmp += min[0];
 	tmp /= max;
 	return tmp;
 }
+
 int max(int min[], int max)
 {
 	int biggest;
-	 biggest = 0;
+
+	biggest = 0;
 	for (int i = 1; i < max; i++)
 	if (biggest <= min[i])
 		biggest = min[0];
