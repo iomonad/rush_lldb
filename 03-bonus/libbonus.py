267116043dab4e237b0024695af50e252230d5bb
Joffrey Janiec <jjaniec@student.42.fr>
2018-06-24 20:04:10 +0200

    add cowsay

--- 03-bonus/libbonus.py
+++ 03-bonus/libbonus.py
@@ -13,8 +13,16 @@ def pwd(debugger, command, result, internal_dict):
 def cd(debugger, command, result, internal_dict):
     os.chdir(command)
 
+def cowsay(debugger, command, result, interal_dict):
+#    r = urllib2.urlopen('https://cowsay.morecode.org/say?format=text&message=' + command)
+#    print r.read()
+    os.system("cowsay " + command)
+
+
 def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('command script add -f libbonus.cmd cmd')
     debugger.HandleCommand('command script add -f libbonus.ls ls')
     debugger.HandleCommand('command script add -f libbonus.pwd pwd')
     debugger.HandleCommand('command script add -f libbonus.cd cd')
+    debugger.HandleCommand('command script add -f libbonus.cowsay cowsay')
+
