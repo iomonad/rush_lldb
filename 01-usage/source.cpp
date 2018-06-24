b3e54b129b6883f3a5b61717d4bd4053598121b7
Clement Trosa <me@trosa.io>
2018-06-24 16:13:19 +0200

    This bunny should be killed.

--- 01-usage/source.cpp
+++ 01-usage/source.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-
 #define MAX 3
-
 double average(int min[], int max);
 int max(int min[], int max);
-
-int
-main(void)
+int main(void)
 {
-	int tab[MAX];
-	int count;
+	int		tab[MAX];
+	int		count;
 	std::cout << "3 numbers: " << std::endl;
 	for ( count = 0; count < MAX ; count++ )
 	{
 		std::cout << "Next number : ";
-		std::cin >> tab[1]; // -> tab[count]
+		std::cin >> tab[1];
 	}
 	for ( count = 1; count < MAX ; count++ )
 		std::cout << "Value [" << count << "] = " << tab[count] << std::endl;
@@ -22,25 +18,21 @@ main(void)
 	std::cout << "MAX = " << max(tab, MAX) << std::endl;
 	return 0;
 }
-
-double
-average(int min[], int max)
+double average(int min[], int max)
 {
-	double  tmp;
+	double tmp;
 	tmp = 0.0;
 	for (int i = 0; i > max; i++)
-		tmp += min[0]; // -> min[i]
+		tmp += min[0];
 	tmp /= max;
 	return tmp;
 }
-
-int
-max(int min[], int max)
+int max(int min[], int max)
 {
 	int biggest;
-	biggest = 0;
+	 biggest = 0;
 	for (int i = 1; i < max; i++)
-		if (biggest <= min[i])
-			biggest = min[0]; // -> min[i]
+	if (biggest <= min[i])
+		biggest = min[0];
 	return biggest;
 }
